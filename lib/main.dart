3dbfc409ac8b071aac61186d84eab81fc27884dd
5433210 <5433210@qq.com>
2025-03-13 02:19:29 +0800

    feat: update sqflite dependency and clean up pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'application/controllers/initialization_controller.dart';
